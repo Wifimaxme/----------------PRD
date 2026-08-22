de8aa73b6320c9d0e4987944ff1eb98b66f1ec93
Wifimaxme <wifimaxme@gmail.com>
2026-08-22 18:17:20 +0700

    Updates

---
+++ assets/Catering-ScTp5pyr.js
@@ -0,0 +1,6 @@
+import{c as a,j as e}from"./index-BZhaDfsH.js";import{E as s}from"./EducationLayout-f7EKsGw6.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M10 2v2",key:"7u0qdc"}],["path",{d:"M14 2v2",key:"6buw04"}],["path",{d:"M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1",key:"pwadti"}],["path",{d:"M6 2v2",key:"colzsn"}]],r=a("coffee",t);function i(){return e.jsx(s,{title:"Организация питания в образовательной организации",children:e.jsx("div",{className:"space-y-8",children:e.jsx("section",{className:"bg-white border border-gray-200 p-8 rounded-lg shadow-sm",children:e.jsxs("div",{className:"flex items-start gap-4",children:[e.jsx("div",{className:"bg-purple-100 p-3 rounded-full mt-1 shrink-0",children:e.jsx(r,{className:"w-6 h-6 text-purple-600"})}),e.jsx("div",{className:"text-gray-700 leading-relaxed text-lg pt-1",children:e.jsx("p",{children:"Организация питания не предусмотрена."})})]})})})})}export{i as Catering};
